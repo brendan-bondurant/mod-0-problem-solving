d9e5e09c6725cd525ead2219acfd3b7a0ef51157
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-08-08 08:29:00 -0600

    Begin challenge 2

---
+++ challenge2.rb
@@ -0,0 +1,14 @@
+# Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+#This needs to INPUT the strings as they are written, and OUTPUT them to be all lowercase
+
+random_capitalization = ["blUeY", "BiNgO", "mUm", "dAD", "I've SEEN ToO maNY epISODes of this SHoW, but At LeAst It IS BETtEr than PEPPA PiG" ]
+
+#If I just wanted to apply this to the words in the above array, I could do this
+
+# random_capitalization.each do |lower_case| 
+#     puts lower_case.tr("A-Z", "a-z")
+# end 
+
+#If I want to make it a method, I would do the following
+def all_lowercase 
