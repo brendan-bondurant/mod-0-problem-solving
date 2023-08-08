fc99dfa2f57fd44ada39b65717f173c41299f2f3
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-08-08 08:37:47 -0600

    Solution to challenge2

--- challenge2.rb
+++ challenge2.rb
@@ -3,6 +3,7 @@
 #This needs to INPUT the strings as they are written, and OUTPUT them to be all lowercase
 
 random_capitalization = ["blUeY", "BiNgO", "mUm", "dAD", "I've SEEN ToO maNY epISODes of this SHoW, but At LeAst It IS BETtEr than PEPPA PiG" ]
+other_words = ["SiLvEr", "IpHoNe", "TyPiNg ThIs WaY rEmInDs Me Of SoCiAl MeDiA iN eArLy 2000'S"]
 
 #If I just wanted to apply this to the words in the above array, I could do this
 
@@ -11,4 +12,11 @@ random_capitalization = ["blUeY", "BiNgO", "mUm", "dAD", "I've SEEN ToO maNY epI
 # end 
 
 #If I want to make it a method, I would do the following
-def all_lowercase 
+def all_lowercase(arr)
+        arr.each do |arr|
+        puts arr.tr!("A-Z", "a-z")
+    end
+end
+
+p all_lowercase(random_capitalization)
+p all_lowercase(other_words)
